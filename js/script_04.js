7f26b097fd5e6010102925f328d6471b908b0063
Alekssey Demchenko <ad@pesochin.net>
2023-08-11 20:50:03 +0300

    Learn JS : Getting and modifying tag content

--- js/script_04.js
+++ js/script_04.js
@@ -1,36 +1,13 @@
 "use strict";
 
-let navList = document.querySelectorAll("li");
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-let a = 0;
+let elem = document.querySelector("h1");
 
-// console.log(navList);
-// console.log(arr);
-//
-for (const value of navList) {
-  console.log(value);
-}
+// console.log(elem.innerHTML);
 
-for (let i = 0; i < arr.length; i++) {
-  console.log(arr[i]);
-}
+// let oldData = elem.innerHTML;
 
-// do {
-//   console.log(a);
-//   a++;
-// } while (a < 3);
-// {
-//   console.log(a);
-//   a++;
-// }
+elem.outerHTML = `<p>Hello World! -tram pam pam</p>`; // замена элемента
 
-let obj = {
-  name: "Ivan",
-  lastName: "Ivanov",
-  age: 25,
-};
+console.log(elem.firstChild); // получение первого дочернего элемента
 
-// for (const key in navList) {
-//   // console.log(navList);
-//   console.log(arr[key]);
-// }
+elem.firstChild.data = "New Comments"; // изменение текста
