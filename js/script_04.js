30b9614a3a3ddfebf93e479bc6f7c000544513f2
Alekssey Demchenko <ad@pesochin.net>
2023-08-11 20:32:09 +0300

    Learn JS : How for of and for in loops work

--- js/script_04.js
+++ js/script_04.js
@@ -2,6 +2,35 @@
 
 let navList = document.querySelectorAll("li");
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let a = 0;
 
-console.log(navList);
-console.log(arr);
+// console.log(navList);
+// console.log(arr);
+//
+for (const value of navList) {
+  console.log(value);
+}
+
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
+
+// do {
+//   console.log(a);
+//   a++;
+// } while (a < 3);
+// {
+//   console.log(a);
+//   a++;
+// }
+
+let obj = {
+  name: "Ivan",
+  lastName: "Ivanov",
+  age: 25,
+};
+
+// for (const key in navList) {
+//   // console.log(navList);
+//   console.log(arr[key]);
+// }
