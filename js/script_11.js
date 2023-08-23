f241f79b983329597e863f9687fbcb1b5af01979
Alekssey Demchenko <ad@pesochin.net>
2023-08-23 21:08:53 +0300

    Learn JS : What is Map

--- js/script_11.js
+++ js/script_11.js
@@ -1,28 +1,41 @@
 "use strict";
 
-const names = [
-  "John",
-  "Bob",
-  "Barry",
-  "Olga",
-  "Ben",
-  "Nancy",
-  "Nancy",
-  "Linda",
-  "Linda",
-];
+// set - коллекция уникальных значений
 
-const uniqueNames = new Set(names);
-// uniqueNames.has("Linda");
-console.log(...uniqueNames);
-
-const name = "Alex";
-const uniqueName = new Set(name);
-console.log(uniqueName);
-
-for (let value of uniqueNames) {
-  console.log(value);
-}
+// const names = [
+//   "John",
+//   "Bob",
+//   "Barry",
+//   "Olga",
+//   "Ben",
+//   "Nancy",
+//   "Nancy",
+//   "Linda",
+//   "Linda",
+// ];
+//
+// const uniqueNames = new Set(names);
+// // uniqueNames.has("Linda");
+// console.log(...uniqueNames);
+//
+// const name = "Alex";
+// const uniqueName = new Set(name);
+// console.log(uniqueName);
+//
+// for (let value of uniqueNames) {
+//   console.log(value);
+// }
 
 // const updatedNames = Array.from(uniqueNames);
 // console.log(updatedNames);
+
+// map - коллекция ключ-значение
+
+const hotel = new Map();
+hotel.set("name", ["Resort Hotel", "Alex", "Motel"]);
+hotel.set("true", "open");
+hotel.set("false", "close");
+const arr = [1, 2, 3];
+hotel.set(arr, "number");
+
+console.log(hotel.has("name"));
