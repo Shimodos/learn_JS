53ac983aca9fb6889c48f2dbef29c4add991d249
Alekssey Demchenko <ad@pesochin.net>
2023-09-18 22:16:59 +0300

    Learn JS : In-depth work with DOM : Event delegation

--- 17-Deepening into the workings of the DOM/start/script.js
+++ 17-Deepening into the workings of the DOM/start/script.js
@@ -30,8 +30,7 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
-console.log(window);
-
+// Прокрутка страницы до элемента
 const btnScroll = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
 
@@ -44,4 +43,61 @@ btnScroll.addEventListener("click", function (e) {
   section1.scrollIntoView({ behavior: "smooth" }); // скролл до элемента  новый способ
 });
 
-console.log(btnScroll.getBoundingClientRect());
+// Page navigation
+// document.querySelectorAll(".nav__link").forEach(function (el) {
+//   el.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute("href");
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+
+// Event delegation - делегирование событийж
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+  console.log(e.target);
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    console.log(id);
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+// const h1 = document.querySelector("h1");
+
+// function alertH1(e) {
+//   alert("Hello");
+//   // h1.removeEventListener("mouseenter", alertH1); // удаляет обработчик события
+// }
+// h1.addEventListener("mouseenter", alertH1); // добавляет обработчик события
+//
+// setTimeout(() => h1.removeEventListener("mouseenter", alertH1), 3000); // удаляет обработчик события через 3 секунды
+
+// const randomInt = (min, max) =>
+//   Math.floor(Math.random() * (max - min + 1) + min);
+
+// Работа с событиями на родителе и потомках (всплытие и погружение)
+// const randomColor = () =>
+//   `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`; // создает рандомный цвет
+//
+// const nav = document.querySelector(".nav"),
+//   navLinks = document.querySelector(".nav__links"),
+//   navLink = document.querySelector(".nav__link");
+//
+// nav.addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor(); // this - это элемент на котором произошло событие
+//   console.log("LINK", e.target, e.currentTarget === this);
+//   // e.stopPropagation(); // останавливает всплытие события
+// });
+//
+// navLinks.addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor(); // this - это элемент на котором произошло событие
+//
+//   // e.stopPropagation(); // останавливает всплытие события
+// });
+//
+// navLink.addEventListener("click", function (e) {
+//   this.style.backgroundColor = randomColor(); // this - это элемент на котором произошло событие
+//   e.stopPropagation(); // останавливает всплытие события
+// });
