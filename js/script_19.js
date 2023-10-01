cfb52266b5c45289cef84d6180c1c2d86e393915
Alekssey Demchenko <ad@pesochin.net>
2023-10-01 20:38:29 +0300

    Learn JS : Object Oriented Programming: Getter and Setter

--- js/script_19.js
+++ js/script_19.js
@@ -1,18 +1,18 @@
 "use strict";
 
-function User(firstName, lastName, age) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.age = age;
-}
-// наследование прототипа от Object
-User.prototype.getFullName = function () {
-  return `${this.firstName} ${this.lastName}`;
-};
-
-const ivan = new User("Ivan", "Ivanov", 20);
-console.log(ivan);
-console.log(ivan.getFullName());
+// function User(firstName, lastName, age) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.age = age;
+// }
+// // наследование прототипа от Object
+// User.prototype.getFullName = function () {
+//   return `${this.firstName} ${this.lastName}`;
+// };
+//
+// const ivan = new User("Ivan", "Ivanov", 20);
+// console.log(ivan);
+// console.log(ivan.getFullName());
 
 // const arr = [];
 // console.log(arr);
@@ -27,16 +27,60 @@ console.log(ivan.getFullName());
 
 // Class declaration (объявление класса) синтаксический сахар (Укрощение синтаксиса)
 class User1 {
-  constructor(firstName, lastName, age) {
+  constructor(firstName, lastName, birthYear) {
     this.firstName = firstName;
     this.lastName = lastName;
-    this.age = age;
+    this.age;
+    this.birthYear = birthYear;
   }
-  getFullName = function () {
-    return `${this.firstName} ${this.lastName}`;
-  };
+  get birthYear() {
+    return new Date().getFullYear() - this.age;
+  }
+  set birthYear(value) {
+    const year = "" + value;
+    if (year.length !== 4) {
+      alert("Год указан не верно");
+      return;
+    }
+    this.age = new Date().getFullYear() - year;
+  }
 }
 
-const ivan1 = new User1("Ivan", "Ivanov", 20);
+const ivan1 = new User1("Ivan", "Ivanov", 1692);
 console.log(ivan1);
-console.log(ivan1.getFullName());
+
+// Getters and Setters
+
+// const user = {
+//   firstName: "Ivan",
+//   lastName: "Ivanov",
+//   age: 20,
+//   get birthYear() {
+//     return new Date().getFullYear() - this.age;
+//   }, // getter - получение значения
+// };
+//
+// console.log(user.birthYear);
+
+class user {
+  constructor(fullName) {
+    this.firstName;
+    this.lastName;
+    this.fullName = fullName;
+  }
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  } // getter - получение значения
+  set fullName(value) {
+    if (value.length < 3) {
+      alert("Имя слишком короткое");
+      return;
+    }
+    const name = value.split(" ");
+    this.firstName = name[0];
+    this.lastName = name[1];
+  } // setter - установка значения
+}
+
+const ivan = new user("Ivan Ivanov");
+console.log(ivan);
