392ee5b8b96aca95fd87fe41cd5fc1c73f0474e3
Alekssey Demchenko <ad@pesochin.net>
2023-10-12 20:07:57 +0300

    Learn JS : Object Oriented Programming: Training Diary : Add Marker to Page

--- 22-Training Diary/start/script.js
+++ 22-Training Diary/start/script.js
@@ -40,10 +40,28 @@ if (navigator.geolocation)
           '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
       }).addTo(map);
 
-      L.marker(coords)
-        .addTo(map)
-        .bindPopup("A pretty CSS popup.<br> Easily customizable.")
-        .openPopup();
+      map.on(
+        "click",
+        function (mapEvent) {
+          console.log(mapEvent);
+          const { lat, lng } = mapEvent.latlng;
+
+          L.marker([lat, lng], { draggable: true })
+            .addTo(map)
+            .bindPopup(
+              L.popup({
+                maxWidth: 250,
+                minWidth: 100,
+                autoClose: true,
+                // closeOnClick: false,
+                className: "mark-popup",
+              }),
+            )
+            .setPopupContent("View Point")
+            .openPopup();
+        },
+        250,
+      );
     },
     function () {
       alert("Could not get your position");
