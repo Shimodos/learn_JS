340a9999b5e6589a0add779c957e55d039cadde1
Alekssey Demchenko <ad@pesochin.net>
2023-10-26 11:54:00 +0300

    Learn JS : Object Oriented Programming: 23-Asynchronous code: How a chained promise works

--- 23-Asynchronous code/start/script.js
+++ 23-Asynchronous code/start/script.js
@@ -4,14 +4,65 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
 ///////////////////////////////////////
-function renderCountry(country) {
-  const request = new XMLHttpRequest();
 
-  request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
-  request.send();
+// Старый способ с использованием XMLHttpRequest
 
-  function renderCards(data, className = "") {
-    const html = `
+// function renderCountry(country) {
+//   const request = new XMLHttpRequest();
+
+//   request.open("GET", `https://restcountries.com/v3.1/name/${country}`);
+//   request.send();
+
+//   function renderCards(data, className = "") {
+//     const html = `
+//     <article class="country ${className}">
+//       <img class="country__img" src="${data.flags.svg}" />
+//       <div class="country__data">
+//       <h3 class="country__name">${data.name.common}</h3>
+//       <h4 class="country__region">${data.region}</h4>
+//       <p class="country__row"><span>👫</span>${data.population}</p>
+//       <p class="country__row"><span>🗣️</span>${
+//         Object.entries(data.languages)[0][1]
+//       }</p>
+//       <p class="country__row"><span>💰</span>${
+//         Object.entries(Object.entries(data.currencies)[0][1])[0][1]
+//       }</p>
+//       </div>
+//     </article>`;
+
+//     countriesContainer.insertAdjacentHTML("beforeend", html);
+//     countriesContainer.style.opacity = 1;
+//   }
+
+//   request.addEventListener("load", function () {
+//     const [data] = JSON.parse(this.responseText);
+//     console.log(data);
+//     const neighbour = data.borders[0];
+//     console.log(neighbour);
+
+//     renderCards(data);
+
+//     const request2 = new XMLHttpRequest();
+//     request2.open("GET", `https://restcountries.com/v3.1/alpha/${neighbour}`);
+//     request2.send();
+
+//     request2.addEventListener("load", function () {
+//       const [data2] = JSON.parse(this.responseText);
+//       console.log(data2);
+
+//       renderCards(data2, "neighbour");
+//     });
+//   });
+// }
+
+// // renderCountry("usa");
+// // renderCountry("ukraine");
+// renderCountry("canada");
+
+// Новый способ с использованием fetch и промисов
+
+function renderCards(data, className = "") {
+  const html = `
     <article class="country ${className}">
       <img class="country__img" src="${data.flags.svg}" />
       <div class="country__data">
@@ -27,31 +78,27 @@ function renderCountry(country) {
       </div>
     </article>`;
 
-    countriesContainer.insertAdjacentHTML("beforeend", html);
-    countriesContainer.style.opacity = 1;
-  }
-
-  request.addEventListener("load", function () {
-    const [data] = JSON.parse(this.responseText);
-    console.log(data);
-    const neighbour = data.borders[0];
-    console.log(neighbour);
-
-    renderCards(data);
-
-    const request2 = new XMLHttpRequest();
-    request2.open("GET", `https://restcountries.com/v3.1/alpha/${neighbour}`);
-    request2.send();
-
-    request2.addEventListener("load", function () {
-      const [data2] = JSON.parse(this.responseText);
-      console.log(data2);
-
-      renderCards(data2, "neighbour");
-    });
-  });
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
 }
 
-// renderCountry("usa");
-// renderCountry("ukraine");
-renderCountry("canada");
+const gatRenderCountry = function (country, className = "") {
+  const request = fetch(`https://restcountries.com/v3.1/name/${country}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      renderCards(data[0], className);
+      const neighbour = data[0].borders[0];
+      console.log(neighbour);
+
+      return fetch(`https://restcountries.com/v3.1/alpha/${neighbour}`);
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      renderCards(data[0], "neighbour");
+    });
+};
+gatRenderCountry("brazil");
